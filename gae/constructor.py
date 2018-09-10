9ad29d743aa0655f36938ba847c927020f900a23
kaize0409 <kaize0409@163.com>
2018-09-10 11:41:14 -0700

    fix bug

--- gae/constructor.py
+++ gae/constructor.py
@@ -28,8 +28,8 @@ def get_model(model_str, placeholders, num_features, num_nodes, features_nonzero
 
 def get_optimizer(model_str, model, placeholders, num_nodes):
     if model_str == 'gcn_ae':
-        opt = OptimizerAE(preds=tf.sparse_tensor_to_dense(model.reconstructions),
-                          labels=placeholders['features'])
+        opt = OptimizerAE(preds=model.reconstructions,
+                          labels=tf.sparse_tensor_to_dense(placeholders['features']))
     elif model_str == 'gcn_vae':
         opt = OptimizerVAE(preds=model.reconstructions,
                            labels=placeholders['features'],
