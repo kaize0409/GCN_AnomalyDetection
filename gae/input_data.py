61bc3b8527bac28e2726743831b256bdb8d32f34
Kaize Ding (Student) <kding9@en4113776l.fulton.ad.asu.edu>
2019-04-19 17:08:18 -0700

    fix data loading

--- gae/input_data.py
+++ gae/input_data.py
@@ -14,36 +14,12 @@ def parse_index_file(filename):
     for line in open(filename):
         index.append(int(line.strip()))
     return index
-# def load_data(dataset):
-#     # load the data: x, tx, allx, graph
-#     names = ['x', 'tx', 'allx', 'graph']
-#     objects = []
-#     for i in range(len(names)):
-#         objects.append(pkl.load(open("data/ind.{}.{}".format(dataset, names[i]))))
-#     x, tx, allx, graph = tuple(objects)
-#     test_idx_reorder = parse_index_file("data/ind.{}.test.index".format(dataset))
-#     test_idx_range = np.sort(test_idx_reorder)
-#
-#     if dataset == 'citeseer':
-#         # Fix citeseer dataset (there are some isolated nodes in the graph)
-#         # Find isolated nodes, add them as zero-vecs into the right position
-#         test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
-#         tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-#         tx_extended[test_idx_range-min(test_idx_range), :] = tx
-#         tx = tx_extended
-#
-#     features = sp.vstack((allx, tx)).tolil()
-#     features[test_idx_reorder, :] = features[test_idx_range, :]
-#     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
-#
-#     return adj, features
-
 
 def load_data(data_source):
     data = scipy.io.loadmat("data/{}.mat".format(data_source))
-    labels = data["Label"]
-    attributes = sp.csr_matrix(data["Attributes"])
-    network = sp.lil_matrix(data["Network"])
+    labels = data["gnd"]
+    attributes = sp.csr_matrix(data["X"])
+    network = sp.lil_matrix(data["A"])
 
     return network, attributes, labels
 
